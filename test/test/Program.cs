fe26129f984f6b29b71c77b6e62f89402909313d
Andreea Stani <stani.andreea@gmail.com>
2022-01-07 18:16:15 +0200

    tema

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -7,6 +7,46 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Alege Operatie:");
+            Console.WriteLine("1.Adunare");
+            Console.WriteLine("2.Scadere");
+            Console.WriteLine("3.Inmultire");
+            Console.WriteLine("1.Impartire");
+            Console.WriteLine("2.Ridicare la putere");
+            Console.WriteLine("3.Radacina patrada");
+            Console.WriteLine("1.Modul de x");
+            Console.WriteLine("2.Par / Impar");
+            Console.WriteLine("3.Prim");
+            Console.WriteLine("3.Oglindit");
+            Console.WriteLine("3.Palindrom");
+            Console.WriteLine("3.SuperPalindrom");
+            Console.WriteLine("3.Afiseaza numerele palindrom si superpalindrom intre 1 si 10000");
+
+            var operatie = Console.ReadLine();
+
+            if (operatie == "1")
+            {
+                Console.WriteLine("A=");
+                int a;
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("B=");
+                int b;
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"A+B={a + b}");
+            }
+            if (operatie == "2")
+            {
+                Console.WriteLine("A=");
+                int a;
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("B=");
+                int b;
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"A-B={a - b}");
+            }
+
         }
     }
 }
