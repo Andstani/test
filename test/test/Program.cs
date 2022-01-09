514c39c73423c574ffaf9bf141c54c3a9a8d217f
Andreea Stani <stani.andreea@gmail.com>
2022-01-09 20:33:08 +0200

    Completat branch operatii. Functioneaza de la 1 la 7

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -14,7 +14,7 @@ namespace test
             Console.WriteLine("3.Inmultire");
             Console.WriteLine("4.Impartire");
             Console.WriteLine("5.Ridicare la putere");
-            Console.WriteLine("6.Radacina patrada");
+            Console.WriteLine("6.Radacina patrata");
             Console.WriteLine("7.Modul de x");
 
             // un branch
@@ -70,7 +70,7 @@ namespace test
             }
             if (operatie == "4")
             {
-                Console.WriteLine("Ai ales operatia de Inpartire. Alege valoarea lui A");
+                Console.WriteLine("Ai ales operatia de Impartire. Alege valoarea lui A");
                 Console.WriteLine("A=");
                 double a;
                 a = Convert.ToDouble(Console.ReadLine());
@@ -82,6 +82,39 @@ namespace test
                 Console.WriteLine($"A/B={a / b}");
 
             }
+            if (operatie == "5")
+            {
+                Console.WriteLine("Ai ales operatia de Ridicare la putere. Alege valoarea lui A");
+                Console.WriteLine("A=");
+                int a;
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" Alege valoarea lui B");
+                Console.WriteLine("B=");
+                int b;
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"A^B={Math.Pow(a,b)}");
+
+            }
+            if (operatie == "6")
+            {
+                Console.WriteLine("Ai ales operatia de aflare a Radacinii patrate. Alege valoarea lui A");
+                Console.WriteLine("A=");
+                int a;
+                a = Convert.ToInt32(Console.ReadLine());
+            
+                Console.WriteLine($"Radacina patrata a lui A={Math.Sqrt(a)}");
+            }
+            if (operatie == "7")
+            {
+                Console.WriteLine("Ai ales operatia de aflare a Valorii absolute (Modul de x). Alege valoarea lui A");
+                Console.WriteLine("A=");
+                double a;
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Valoarea absoluta a lui A={Math.Abs(a)}");
+            }
+
         }
     }
 }
